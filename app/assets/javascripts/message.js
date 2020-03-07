42d47d4ca389d1ef91e2a1b5ed54a5a12b7e0767
poke3104 <sevenchild_trial@yahoo.co.jp>
2020-03-07 11:56:09 +0900

    html修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -91,6 +91,6 @@ $(function(){
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    // setInterval(reloadMessages, 7000);
+    setInterval(reloadMessages, 7000);
   }
 });
