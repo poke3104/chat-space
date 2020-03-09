ad2adad5dd9568871e2598c5e2613d2d359b6719
poke3104 <sevenchild_trial@yahoo.co.jp>
2020-03-09 20:39:39 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # protect_from_forgery except: :search #対策コードを追加
   before_action :authenticate_user!
   before_action :configure_permitted_parameters,if: :devise_controller?
 
